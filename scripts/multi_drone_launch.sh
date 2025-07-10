e05ed8c8640b07a467b4d4c1bebf1e3a28a88216
Himanshu069 <078bme014.himanshu@pcampus.edu.np>
2025-07-10 18:49:53 +0545

    change in shell script to make it more robust

--- scripts/multi_drone_launch.sh
+++ scripts/multi_drone_launch.sh
@@ -1,32 +1,55 @@
 #!/bin/bash
+echo "=== Cleaning up old processes ==="
+pkill -f px4
+pkill -f gz
+pkill -f ros2
+pkill -f MicroXRCEAgent
+sleep 2  
 
-echo "=== Starting QGroundControl ==="
-gnome-terminal -- bash -c "cd ~ && ./QGroundControl-x86_64.AppImage; exec bash"
-sleep 3
+echo "=== Clearing old PX4 logs ==="
+> ~/px4_instance1.log
+> ~/px4_instance2.log
+
 
 cd ~/PX4-Autopilot || exit 1
 
 echo "=== Starting PX4 SITL Instance 1 ==="
-gnome-terminal -- bash -c "PX4_SIM_INSTANCE=1 MAV_SYS_ID=2 PX4_GZ_MODEL_POSE='0,1,0' make px4_sitl gz_x500 | tee ~/px4_instance1.log; exec bash"
+gnome-terminal -- bash -c "PX4_SIM_MODEL=gz_x500 make px4_sitl gz_x500 | tee ~/px4_instance1.log; exec bash"
 
 echo "=== Starting PX4 SITL Instance 2 ==="
-gnome-terminal -- bash -c "PX4_GZ_STANDALONE=1 PX4_SIM_INSTANCE=2 MAV_SYS_ID=3 PX4_GZ_MODEL_POSE='0,2,0' ./build/px4_sitl_default/bin/px4 -i 2 | tee ~/px4_instance2.log; exec bash"
+gnome-terminal -- bash -c "PX4_GZ_STANDALONE=1 PX4_GZ_MODEL_POSE="0,1" PX4_SIM_MODEL=gz_x500 ./build/px4_sitl_default/bin/px4 -i 9 | tee ~/px4_instance2.log; exec bash"
+
+sleep 3
+
+echo "=== Starting QGroundControl ==="
+gnome-terminal -- bash -c "cd ~ && ./QGroundControl-x86_64.AppImage; exec bash"
+
+sleep 10
 
 echo "=== Starting Micro XRCE-DDS Agent ==="
 gnome-terminal -- bash -c "MicroXRCEAgent udp4 -p 8888; exec bash"
-sleep 5
+sleep 3
 
-echo "=== Waiting for PX4 Instance 1 Ready for takeoff ==="
-while ! grep -q "Ready for takeoff!" ~/px4_instance1.log; do sleep 1; done
+(
+    echo "=== Waiting for PX4 Instance 1 Ready for takeoff ==="
+    while true; do
+        if grep -q "Ready for takeoff!" ~/px4_instance1.log; then
+            echo "=== PX4 Instance 1 is ready ==="
+            gnome-terminal -- bash -c "source /opt/ros/jazzy/setup.bash && cd ~/cslam_ws && source install/setup.bash && ros2 run px4_ros_com offboard_control --ros-args -p px4_instance:=0; exec bash"
+            break
+        fi
+        sleep 1
+    done
+)&
 
-echo "=== Waiting for PX4 Instance 2 Ready for takeoff ==="
-while ! grep -q "Ready for takeoff!" ~/px4_instance2.log; do sleep 1; done
-
-
-
-echo "=== Starting ROS2 offboard_control node for instance 1 ==="
-gnome-terminal -- bash -c "source /opt/ros/jazzy/setup.bash && cd ~/cslam_ws && source install/setup.bash && ros2 run px4_ros_com offboard_control; exec bash"
-sleep 5
-
-echo "=== Starting ROS2 offboard_control node for instance 2 ==="
-gnome-terminal -- bash -c "source /opt/ros/jazzy/setup.bash && cd ~/cslam_ws && source install/setup.bash && ros2 run px4_ros_com offboard_control --ros-args -p px4_instance:=2; exec bash"
+(
+    echo "=== Waiting for PX4 Instance 2 Ready for takeoff ==="
+    while true; do
+        if grep -q "Ready for takeoff!" ~/px4_instance2.log; then
+            echo "=== PX4 Instance 2 is ready ==="
+            gnome-terminal -- bash -c "source /opt/ros/jazzy/setup.bash && cd ~/cslam_ws && source install/setup.bash && ros2 run px4_ros_com offboard_control --ros-args -p px4_instance:=9; exec bash"
+            break
+        fi
+        sleep 1
+    done
+)&
